3c658e486ca7e60f6955c1dbda5cc7f242fe62ce
Nicolas Zapata Rm <zapatanicolas1@gmail.com>
2023-05-23 10:48:10 -0500

    Se agrega archivos de grafico main.py & charts.py,edicion .gitignore

---
+++ charts/charts.py
@@ -0,0 +1,12 @@
+
+import matplotlib.pyplot as plt
+
+def generate_pie_chart():
+     labels = ['A','B','C']
+     values = [200,300,1500]
+
+     fig, ax = plt.subplots()
+     ax.pie(values, labels = labels)
+     plt.savefig('chart.png')
+     plt.close()
+
